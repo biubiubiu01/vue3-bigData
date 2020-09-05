9b3772284a99aace7135c2a44466f85857f52671
biubiubiu01 <1979420758@qq.com>
2020-09-06 00:52:19 +0800

    修改mock

--- mock/index.js
+++ mock/index.js
@@ -1,14 +1,25 @@
 const Mock = require("mockjs");
-const qs = require("qs");
 const { param2Obj } = require("./utils");
 
 const chart = require("./chart");
+// const role = require('./role')
+// const article = require('./article')
+// const search = require('./remote-search')
 
-const mocks = [...chart];
+const mocks = [
+  ...chart,
+  // ...role,
+  // ...article,
+  // ...search
+];
 
+// for front mock
+// please use it cautiously, it will redefine XMLHttpRequest,
+// which will cause many of your third-party libraries to be invalidated(like progress event).
 function mockXHR() {
+  // mock patch
+  // https://github.com/nuysoft/Mock/issues/300
   Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send;
-
   Mock.XHR.prototype.send = function() {
     if (this.custom.xhr) {
       this.custom.xhr.withCredentials = this.withCredentials || false;
@@ -25,9 +36,10 @@ function mockXHR() {
       let result = null;
       if (respond instanceof Function) {
         const { body, type, url } = options;
+        // https://expressjs.com/en/4x/api.html#req
         result = respond({
           method: type,
-          body: qs.parse(body),
+          body: JSON.parse(body),
           query: param2Obj(url),
         });
       } else {
@@ -40,8 +52,8 @@ function mockXHR() {
   for (const i of mocks) {
     Mock.mock(
       new RegExp(i.url),
-      i.type || "get"
-      // XHR2ExpressReqWrap(i.response)
+      i.type || "get",
+      XHR2ExpressReqWrap(i.response)
     );
   }
 }
--- mock/mock-server.js
+++ mock/mock-server.js
@@ -3,18 +3,12 @@ const bodyParser = require("body-parser");
 const chalk = require("chalk");
 const path = require("path");
 const Mock = require("mockjs");
+
 const mockDir = path.join(process.cwd(), "mock");
 
-/**
- *
- * @param app
- * @returns {{mockStartIndex: number, mockRoutesLength: number}}
- */
-const registerRoutes = (app) => {
+function registerRoutes(app) {
   let mockLastIndex;
-  const {
-    mocks
-  } = require("./index.js");
+  const { mocks } = require("./index.js");
   const mocksForServer = mocks.map((route) => {
     return responseFake(route.url, route.type, route.response);
   });
@@ -27,38 +21,33 @@ const registerRoutes = (app) => {
     mockRoutesLength: mockRoutesLength,
     mockStartIndex: mockLastIndex - mockRoutesLength,
   };
-};
+}
 
-/**
- *
- * @param url
- * @param type
- * @param respond
- * @returns {{response(*=, *=): void, type: (*|string), url: RegExp}}
- */
+function unregisterRoutes() {
+  Object.keys(require.cache).forEach((i) => {
+    if (i.includes(mockDir)) {
+      delete require.cache[require.resolve(i)];
+    }
+  });
+}
+
+// for mock server
 const responseFake = (url, type, respond) => {
   return {
     url: new RegExp(`${process.env.VUE_APP_BASE_API}${url}`),
     type: type || "get",
     response(req, res) {
-      res.status(200);
-      if (JSON.stringify(req.body) !== "{}") {
-        console.log(chalk.green(`> 请求地址：${req.path}`));
-        console.log(chalk.green(`> 请求参数：${JSON.stringify(req.body)}\n`));
-      } else {
-        console.log(chalk.green(`> 请求地址：${req.path}\n`));
-      }
+      console.log("request invoke:" + req.path);
       res.json(
         Mock.mock(respond instanceof Function ? respond(req, res) : respond)
       );
     },
   };
 };
-/**
- *
- * @param app
- */
+
 module.exports = (app) => {
+  // parse app.body
+  // https://expressjs.com/en/4x/api.html#req.body
   app.use(bodyParser.json());
   app.use(
     bodyParser.urlencoded({
@@ -67,29 +56,35 @@ module.exports = (app) => {
   );
 
   const mockRoutes = registerRoutes(app);
-  let mockRoutesLength = mockRoutes.mockRoutesLength;
-  let mockStartIndex = mockRoutes.mockStartIndex;
+  var mockRoutesLength = mockRoutes.mockRoutesLength;
+  var mockStartIndex = mockRoutes.mockStartIndex;
+
+  // watch files, hot reload mock server
   chokidar
     .watch(mockDir, {
       ignored: /mock-server/,
       ignoreInitial: true,
     })
-    .on("all", (event) => {
+    .on("all", (event, path) => {
       if (event === "change" || event === "add") {
         try {
+          // remove mock routes stack
           app._router.stack.splice(mockStartIndex, mockRoutesLength);
 
-          Object.keys(require.cache).forEach((item) => {
-            if (item.includes(mockDir)) {
-              delete require.cache[require.resolve(item)];
-            }
-          });
+          // clear routes cache
+          unregisterRoutes();
+
           const mockRoutes = registerRoutes(app);
           mockRoutesLength = mockRoutes.mockRoutesLength;
           mockStartIndex = mockRoutes.mockStartIndex;
-        } catch (error) {
 
-          console.log(chalk.red(error));
+          console.log(
+            chalk.magentaBright(
+              `\n > Mock Server hot reload success! changed  ${path}`
+            )
+          );
+        } catch (error) {
+          console.log(chalk.redBright(error));
         }
       }
     });
