8c573a08d565ecf2a84a3cf447381f979407d016
gist006 <1979420758@qq.com>
2020-09-06 19:55:04 +0800

    删除

--- UI/components/index.js
+++
@@ -1,2 +0,0 @@
-export {default as ZRow}from './layout/row/index'
-export {default as ZCol}from './layout/col/index'
--- src/views/homepage/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as cityCount } from "./cityCount.vue";
-export { default as typeCount } from "./typeCount.vue";
-export { default as scatterMap } from "./scatterMap.vue";
-export { default as wordChart } from "./wordChart.vue";
-export { default as funnelChart } from "./funnelChart.vue";
-export { default as lineChart } from "./lineChart.vue";
-export { default as liquidChart } from "./liquidChart.vue";
